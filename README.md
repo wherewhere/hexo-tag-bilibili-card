df49db3cbd5cb77fa182faa373764618843ef721
wherewhere <qq2518766683@outlook.com>
2024-05-31 18:27:10 +0800

    使用代码生成内容

--- README.md
+++ README.md
@@ -14,12 +14,19 @@
 npm i hexo-tag-bilibili-card
 ```
 
-<!-- 
 ## 预览
 
-<script src="./components/bilibili-card/bilibili-card.js"></script>
+<p>
+    <script src="https://cdn.jsdelivr.net/gh/wherewhere/hexo-tag-bilibili-card@main/components/bilibili-card/bilibili-card.js"></script>
+    <bilibili-card vid="BV1y54y1a768" title="【UWP】手把手教你安装 UWP 安装包" upper="where-where" cover="http://i2.hdslb.com/bfs/archive/41bc750cb5011bb036e008a716a89158c7eb7bb5.jpg" duration="05:21" views="2.2万" danmakus="4" image-proxy="https://images.weserv.nl/?url="></bilibili-card>
+</p>
+
+如果这里什么也没有，可以复制以下代码到 HTML 文件中查看效果
+
+```html
+<script src="https://cdn.jsdelivr.net/gh/wherewhere/hexo-tag-bilibili-card@main/components/bilibili-card/bilibili-card.js"></script>
 <bilibili-card aid="BV1y54y1a768" title="【UWP】手把手教你安装 UWP 安装包" upper="where-where" cover="http://i2.hdslb.com/bfs/archive/41bc750cb5011bb036e008a716a89158c7eb7bb5.jpg" duration="05:21" views="2.2万" danmakus="4" image-proxy="https://images.weserv.nl/?url="></bilibili-card>
--->
+```
 
 ## 使用
 
@@ -29,13 +36,13 @@ npm i hexo-tag-bilibili-card
 {% bilibili_card your_video_id %}
 ```
 
-`your_video_id` 是 B站 的 BV号 或 AV号。
+`your_video_id` 是 B站 的 BV号 或 AV号
 
-然后你就可以看见文章中的卡片了。
+然后你就可以看见文章中的卡片了
 
 ## 配置
 
-由于 B站 图片地址的跨域限制，因此需要配置图片代理，目前未找到大陆地区较为稳定的服务，默认使用的 `https://images.weserv.nl/?url=`，部分地区受到了 DNS 污染及反应过慢。建议参考 [rsstt-img-relay](https://github.com/Rongronggg9/rsstt-img-relay) 自建服务。
+由于 B站 图片地址的跨域限制，因此需要配置图片代理，目前未找到大陆地区较为稳定的服务，默认使用的 `https://images.weserv.nl/?url=`，部分地区受到了 DNS 污染及反应过慢。建议参考 [rsstt-img-relay](https://github.com/Rongronggg9/rsstt-img-relay) 自建服务
 
 ```yaml
 bilibili_card:
--- components/bilibili-card/bilibili-card.js
+++ components/bilibili-card/bilibili-card.js
@@ -10,87 +10,150 @@ function getScript(scriptName) {
     }
 }
 
-function cardTemplate(imageProxy, { v_id, v_cover, v_time, v_title, v_playview, v_danmaku, v_upname }) {
-    return `<link rel="stylesheet" href="https://cdn.staticfile.net/font-awesome/4.7.0/css/font-awesome.min.css">
-            <div class="default-flex video-holder normal-card">
-                <a class="default-flex" href="//www.bilibili.com/video/${v_id}" target="_blank">
-                    <div class="disable-event cover-box">
-                        <div class="cover-img"
-                            style="background-image: url(${imageProxy}${v_cover})">
-                        </div>
-                        <div class="video-mask">
-                            <i class="video-icon fa fa-youtube-play"></i>
-                            <div class="video-sub">
-                                <div class="video-duration">${v_time}</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="disable-event video-content-container">
-                        <p class="double-ellipsis video-title">${v_title}</p>
-                        <div class="video-card-info">
-                            <div class="cover-info-item">
-                                <i class="iconfont fa fa-youtube-play"></i>
-                                <span class="single-ellipsis num-info">${v_playview}</span>
-                            </div>
-                            <div class="cover-info-item">
-                                <i class="iconfont fa fa-list-alt"></i>
-                                <span class="single-ellipsis num-info">${v_danmaku}</span>
-                            </div>
-                        </div>
-                        <div class="video-card-bottom">
-                            <label class="card-text-label">视频</label>
-                            <div class="view-flex default-flex align-center slim-author-info">
-                                <label class="icon-up">UP</label>
-                                <span class="view-flex single-ellipsis author-name">${v_upname}</span>
-                            </div>
-                        </div>
-                    </div>
-                </a>
-            </div>`;
-}
+const defaultTitle = "哔哩哔哩 (゜-゜)つロ 干杯~";
+const defaultUpper = "2233";
+const defaultDuration = "??:??";
+const defaultText = "???";
+const defaultProxy = "https://images.weserv.nl/?url=";
 
 class BilibiliCard extends HTMLElement {
     static get observedAttributes() {
         return ['aid', 'title', 'upper', 'cover', 'duration', 'views', 'danmakus', 'image-proxy'];
     }
 
+    contents = {
+        link: null,
+        cover: null,
+        duration: null,
+        title: null,
+        views: null,
+        danmakus: null,
+        author: null
+    }
+
     constructor() {
         super();
+
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+        
+        const css = document.createElement("link");
+        css.rel = "stylesheet";
+        css.href = script.src.substring(0, script.src.lastIndexOf('.')) + ".css";
+        shadowRoot.appendChild(css);
+
+        const awesome = document.createElement("link");
+        awesome.rel = "stylesheet";
+        awesome.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css";
+        shadowRoot.appendChild(awesome);
+
+        const card = document.createElement('div');
+        card.className = 'default-flex video-holder normal-card';
+        shadowRoot.appendChild(card);
+
+        const link = document.createElement("a");
+        link.className = "default-flex";
+        link.target = "_blank";
+        card.appendChild(link);
+        this.contents.link = link;
+
+        const cover_box = document.createElement('div');
+        cover_box.className = 'disable-event cover-box';
+        link.appendChild(cover_box);
+
+        const cover = document.createElement("div");
+        cover.className = "cover-img";
+        cover_box.appendChild(cover);
+        this.contents.cover = cover;
+
+        const mask = document.createElement('div');
+        mask.className = 'video-mask';
+        mask.innerHTML = `<i class="video-icon fa-brands fa-bilibili"></i>`;
+        cover_box.appendChild(mask);
+        
+        const video_sub = document.createElement('div');
+        video_sub.className = 'video-sub';
+        mask.appendChild(video_sub);
+
+        const duration = document.createElement('div');
+        duration.className = 'video-duration';
+        video_sub.appendChild(duration);
+        this.contents.duration = duration;
+
+        const content = document.createElement('div');
+        content.className = 'disable-event video-content-container';
+        link.appendChild(content);
+
+        const title = document.createElement('p');
+        title.className = 'double-ellipsis video-title';
+        content.appendChild(title);
+        this.contents.title = title;
+
+        const info = document.createElement('div');
+        info.className = 'video-card-info';
+        content.appendChild(info);
+
+        const views_box = document.createElement('div');
+        views_box.className = 'cover-info-item';
+        views_box.innerHTML = `<i class="iconfont fa-brands fa-youtube"></i>`;
+        info.appendChild(views_box);
+
+        const views = document.createElement('span');
+        views.className = 'single-ellipsis num-info';
+        views_box.appendChild(views);
+        this.contents.views = views;
+
+        const danmakus_box = document.createElement('div');
+        danmakus_box.className = 'cover-info-item';
+        danmakus_box.innerHTML = `<i class="iconfont fa fa-list-alt"></i>`;
+        info.appendChild(danmakus_box);
+
+        const danmakus = document.createElement('span');
+        danmakus.className = 'single-ellipsis num-info';
+        danmakus_box.appendChild(danmakus);
+        this.contents.danmakus = danmakus;
+
+        const bottom = document.createElement('div');
+        bottom.className = 'video-card-bottom';
+        bottom.innerHTML = `<label class="card-text-label">视频</label>`;
+        content.appendChild(bottom);
+
+        const author_box = document.createElement('div');
+        author_box.className = 'view-flex default-flex align-center slim-author-info';
+        author_box.innerHTML = `<label class="icon-up">UP</label>`;
+        bottom.appendChild(author_box);
+
+        const author = document.createElement('span');
+        author.className = 'view-flex single-ellipsis author-name';
+        author_box.appendChild(author);
+        this.contents.author = author;
     }
 
     connectedCallback() {
-        const link = document.createElement("link");
-        link.rel = "stylesheet";
-        link.href = script.src.substring(0, script.src.lastIndexOf('.')) + ".css";
-        const shadowRoot = this.attachShadow({ mode: 'open' });
-        shadowRoot.innerHTML = cardTemplate(this.imageProxy, {
-            v_id: this.aid,
-            v_cover: this.cover,
-            v_time: this.duration,
-            v_title: this.title,
-            v_playview: this.views,
-            v_danmaku: this.danmakus,
-            v_upname: this.upper
-        });
-        shadowRoot.appendChild(link);
+        this.contents.link.href = `//www.bilibili.com/video/${this.vid}`;
+        this.contents.cover.style.backgroundImage = `url(${this.imageProxy}${this.cover})`;
+        this.contents.duration.textContent = this.duration;
+        this.contents.title.textContent = this.title;
+        this.contents.views.textContent = this.views;
+        this.contents.danmakus.textContent = this.danmakus;
+        this.contents.author.textContent = this.upper;
     }
 
-    get aid() {
-        return this.getAttribute('aid');
+    get vid() {
+        return this.getAttribute('vid');
     }
-    set aid(value) {
-        this.setAttribute('aid', value);
+    set vid(value) {
+        this.setAttribute('vid', value);
     }
 
     get title() {
-        return this.getAttribute('title');
+        return this.getAttribute('title') ?? defaultTitle;
     }
     set title(value) {
         this.setAttribute('title', value);
     }
 
     get upper() {
-        return this.getAttribute('upper');
+        return this.getAttribute('upper') ?? defaultUpper;
     }
     set upper(value) {
         this.setAttribute('upper', value);
@@ -104,28 +167,28 @@ class BilibiliCard extends HTMLElement {
     }
 
     get duration() {
-        return this.getAttribute('duration');
+        return this.getAttribute('duration') ?? defaultDuration;
     }
     set duration(value) {
         this.setAttribute('duration', value);
     }
 
     get views() {
-        return this.getAttribute('views');
+        return this.getAttribute('views') ?? defaultText;
     }
     set views(value) {
         this.setAttribute('views', value);
     }
 
     get danmakus() {
-        return this.getAttribute('danmakus');
+        return this.getAttribute('danmakus') ?? defaultText;
     }
     set danmakus(value) {
         this.setAttribute('danmakus', value);
     }
 
     get imageProxy() {
-        return this.getAttribute('image-proxy');
+        return this.getAttribute('image-proxy') ?? defaultProxy;
     }
     set imageProxy(value) {
         this.setAttribute('image-proxy', value);
