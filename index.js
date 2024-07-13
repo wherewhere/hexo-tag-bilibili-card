a15961e1a83beb0f9f8e07599c264af0480d7b0f
wherewhere <qq2518766683@outlook.com>
2024-07-13 23:24:56 +0800

    现在可以正确解析动态了
    优化方法名称

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const fs = require("hexo-fs");
 const path = require("path");
-const createCard = require("./lib/create-card.js");
+const createCardAsync = require("./lib/create-card.js");
 const js = hexo.extend.helper.get("js").bind(hexo);
 
 function setSettings(obj, set) {
@@ -76,6 +76,6 @@ hexo.extend.generator.register("bilibili_card_asset", () => [
     }
 ]);
 
-hexo.extend.tag.register("bilibili_card", args => createCard(bilibili_card.image_proxy, args, hexo.log), { async: true });
+hexo.extend.tag.register("bilibili_card", args => createCardAsync(bilibili_card.image_proxy, args, hexo.log), { async: true });
 
 bilibili_card.inject_layouts.forEach(layout => hexo.extend.injector.register("head_end", () => js("/components/bilibili-card/bilibili-card.js"), layout));
