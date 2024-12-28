1c327a9d20371c35dec9c74d06ab38ccb6c43121
wherewhere <qq2518766683@outlook.com>
2024-12-28 17:05:00 +0800

    修正 polyfill

--- components/bilibili-card-builder/bilibili-card-builder.js
+++ components/bilibili-card-builder/bilibili-card-builder.js
@@ -30,19 +30,19 @@
     }
 
     if (typeof Array.prototype.includes !== "function") {
-        Array.prototype.includes = value => this.indexOf(value) !== -1;
+        Array.prototype.includes = function (value) { return this.indexOf(value) !== -1; }
     }
 
     if (typeof String.prototype.trimStart !== "function") {
         if (typeof String.prototype.trimLeft !== "function") {
-            String.prototype.trimLeft = () => this.replace(/^[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+/, '');
+            String.prototype.trimLeft = function () { return this.replace(/^[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+/, ''); }
         }
         String.prototype.trimStart = String.prototype.trimLeft;
     }
 
     if (typeof String.prototype.trimEnd !== "function") {
         if (typeof String.prototype.trimRight !== "function") {
-            String.prototype.trimRight = () => this.replace(/[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+$/, '');
+            String.prototype.trimRight = function () { return this.replace(/[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+$/, ''); }
         }
         String.prototype.trimEnd = String.prototype.trimRight;
     }
