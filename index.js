b64b7da1f018b45b24ee6b429579ef82ba576453
wherewhere <qq2518766683@outlook.com>
2025-01-26 10:42:56 +0800

    现在可以按需导入资源了

--- index.js
+++ index.js
@@ -1,48 +1,39 @@
 /* global hexo */
 "use strict";
 
-if (typeof hexo !== "undefined") {
-    const fs = require("hexo-fs");
-    const path = require("path");
-    const createCardAsync = require("./lib/create-card.js");
+/** @typedef {import("@types/hexo")} */
 
-    function setSettings(obj, set) {
+const path = require("path");
+const createCardAsync = require("./lib/create-card.js");
+
+function setSettings(obj, set) {
+    if (typeof set === "undefined") {
+        return;
+    }
+    else if (set === null) {
+        return;
+    }
+    else if (typeof obj !== "object" || obj instanceof Array) {
+        obj = set;
+        return;
+    }
+    const keys = Object.keys(set);
+    if (!keys.length) {
+        return;
+    }
+    function setKey(obj, set) {
         if (typeof set === "undefined") {
-            return;
+            return false;
         }
         else if (set === null) {
-            return;
+            return false;
         }
         else if (typeof obj !== "object" || obj instanceof Array) {
-            obj = set;
-            return;
+            return true;
         }
         const keys = Object.keys(set);
         if (!keys.length) {
-            return;
-        }
-        function setKey(obj, set) {
-            if (typeof set === "undefined") {
-                return false;
-            }
-            else if (set === null) {
-                return false;
-            }
-            else if (typeof obj !== "object" || obj instanceof Array) {
-                return true;
-            }
-            const keys = Object.keys(set);
-            if (!keys.length) {
-                return true;
-            }
-            keys.forEach(key => {
-                if (setKey(obj[key], set[key])) {
-                    if (typeof set[key] !== "undefined") {
-                        obj[key] = set[key];
-                    }
-                }
-            });
-            return false;
+            return true;
         }
         keys.forEach(key => {
             if (setKey(obj[key], set[key])) {
@@ -51,88 +42,119 @@ if (typeof hexo !== "undefined") {
                 }
             }
         });
+        return false;
     }
+    keys.forEach(key => {
+        if (setKey(obj[key], set[key])) {
+            if (typeof set[key] !== "undefined") {
+                obj[key] = set[key];
+            }
+        }
+    });
+}
 
-    const bilibili_card = {
-        enable: true,
-        image_proxy: undefined,
-        inject_layouts: ["default"],
-        mode: "system"
-    };
+const bilibili_card = {
+    enable: true,
+    image_proxy: undefined,
+    inject_layouts: ["default"],
+    generator_assets: ["default"],
+    mode: "system"
+};
 
-    setSettings(bilibili_card, hexo.config.bilibili_card);
+setSettings(bilibili_card, hexo.config.bilibili_card);
 
-    if (!bilibili_card?.enable) {
-        return;
-    }
+if (!bilibili_card.enable) {
+    return;
+}
 
-    const isComponent = bilibili_card?.mode == "component";
+const generator_assets = new Set(bilibili_card.generator_assets);
+if (generator_assets.has("component")) {
+    generator_assets.delete("component");
+    generator_assets.add("components/bilibili-card/bilibili-card.js");
+}
+if (generator_assets.has("system")) {
+    generator_assets.delete("system");
+    generator_assets.add("components/bilibili-card/bilibili-card.css");
+}
+if (generator_assets.has("light")) {
+    generator_assets.delete("light");
+    generator_assets.add("components/bilibili-card/bilibili-card.light.css");
+}
+if (generator_assets.has("dark")) {
+    generator_assets.delete("dark");
+    generator_assets.add("components/bilibili-card/bilibili-card.dark.css");
+}
+if (generator_assets.has("fluent")) {
+    generator_assets.delete("fluent");
+    generator_assets.add("components/bilibili-card/bilibili-card.fluent.css");
+}
+if (generator_assets.has("windose")) {
+    generator_assets.delete("windose");
+    generator_assets.add("components/bilibili-card/bilibili-card.windose.css");
+}
+if (generator_assets.has("builder")) {
+    generator_assets.delete("builder");
+    generator_assets.add("components/bilibili-card-builder/bilibili-card-builder.js");
+}
+if (generator_assets.has("message")) {
+    generator_assets.delete("message");
+    generator_assets.add("components/bilibili-card-message/bilibili-card-message.js");
+}
+generator_assets.delete("default");
 
-    hexo.extend.generator.register("bilibili_card_asset", () => isComponent
-        ? [{
-            path: "components/bilibili-card/bilibili-card.js",
-            data: () => fs.createReadStream(path.resolve(__dirname, "./components/bilibili-card", "bilibili-card.js"))
-        },
-        {
-            path: "components/bilibili-card/bilibili-card.css",
-            data: () => fs.createReadStream(path.resolve(__dirname, "./components/bilibili-card", "bilibili-card.css"))
-        },
-        {
-            path: "components/bilibili-card/bilibili-card.dark.css",
-            data: () => fs.createReadStream(path.resolve(__dirname, "./components/bilibili-card", "bilibili-card.dark.css"))
-        },
-        {
-            path: "components/bilibili-card/bilibili-card.light.css",
-            data: () => fs.createReadStream(path.resolve(__dirname, "./components/bilibili-card", "bilibili-card.light.css"))
-        }]
-        : [{
-            path: "css/bilibili-card.css",
-            data: () => fs.createReadStream(path.resolve(__dirname, (() => {
-                const baseUrl = "./components/bilibili-card/bilibili-card";
-                switch (bilibili_card.mode.toString().toLowerCase()) {
-                    case '1':
-                    case "light":
-                        return `${baseUrl}.light.css`;
-                    case '2':
-                    case "dark":
-                        return `${baseUrl}.dark.css`;
-                    case '0':
-                    case "auto":
-                    case "system":
-                    case "default":
-                        return `${baseUrl}.css`;
-                    case "fd":
-                    case "fd2":
-                    case "fluent":
-                    case "fluentui":
-                        return `${baseUrl}.fluent.css`;
-                    case "windose":
-                        return `${baseUrl}.windose.css`;
-                    case "-1":
-                    case "none":
-                        return '';
-                    default:
-                        return theme;
-                }
-            })()))
-        }]
-    );
+const isComponent = bilibili_card.mode == "component";
 
-    hexo.extend.tag.register("bilibili_card", args => createCardAsync(bilibili_card.image_proxy, args, isComponent, hexo.log), { async: true });
-
-    if (isComponent) {
-        const js = hexo.extend.helper.get("js").bind(hexo);
-        bilibili_card.inject_layouts.forEach(layout => hexo.extend.injector.register("head_end", () => js("/components/bilibili-card/bilibili-card.js"), layout));
-    }
-    else {
-        const css = hexo.extend.helper.get("css").bind(hexo);
-        bilibili_card.inject_layouts.forEach(layout => hexo.extend.injector.register("head_end", () => css("/css/bilibili-card.css"), layout));
-    }
+if (isComponent) {
+    generator_assets.add("components/bilibili-card/bilibili-card.js");
+    generator_assets.add("components/bilibili-card/bilibili-card.css");
+    generator_assets.add("components/bilibili-card/bilibili-card.dark.css");
+    generator_assets.add("components/bilibili-card/bilibili-card.light.css");
+    const js = hexo.extend.helper.get("js").bind(hexo);
+    bilibili_card.inject_layouts.forEach(layout => hexo.extend.injector.register("head_end", () => js("/components/bilibili-card/bilibili-card.js"), layout));
 }
 else {
-    module.exports = {
-        BiliBiliCard: require("./components/bilibili-card/bilibili-card.js"),
-        bilibiliCardMessage: require("./components/bilibili-card-message/bilibili-card-message.js"),
-        bilibiliCardBuilder: require("./components/bilibili-card-builder/bilibili-card-builder.js"),
+    let theme = bilibili_card.mode;
+    switch (theme.toString().toLowerCase()) {
+        case '1':
+        case "light":
+            theme = "components/bilibili-card/bilibili-card.light.css";
+            break;
+        case '2':
+        case "dark":
+            theme = "components/bilibili-card/bilibili-card.dark.css";
+            break;
+        case '0':
+        case "auto":
+        case "system":
+        case "default":
+            theme = "components/bilibili-card/bilibili-card.css";
+            break;
+        case "fd":
+        case "fd2":
+        case "fluent":
+        case "fluentui":
+            theme = "components/bilibili-card/bilibili-card.fluent.css";
+            break;
+        case "windose":
+            theme = "components/bilibili-card/bilibili-card.windose.css";
+            break;
+        case "-1":
+        case "none":
+            theme = '';
+            break;
+        default:
+            break;
     }
+    generator_assets.add(theme);
+    const css = hexo.extend.helper.get("css").bind(hexo);
+    bilibili_card.inject_layouts.forEach(layout => hexo.extend.injector.register("head_end", () => css(path.posix.join('/', theme)), layout));
 }
+
+hexo.extend.generator.register("bilibili_card_asset", () => [...generator_assets].map(asset => {
+    return {
+        path: asset,
+        data: () => hexo.render.render({ path: path.resolve(__dirname, asset) })
+    }
+}));
+
+hexo.extend.tag.register("bilibili_card", args => createCardAsync(bilibili_card.image_proxy, args, isComponent, hexo.log), { async: true });
